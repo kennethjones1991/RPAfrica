fb226b490bc59b7428add9d98ed2c8d4215ce4a3
Kenneth Jones <kennethjones1991@outlook.com>
2020-12-31 01:51:39 -0700

    Created initial project

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Kenneth Jones on 12/31/20.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
