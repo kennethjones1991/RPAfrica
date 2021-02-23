57f83056d3c14f81b452cccf4543654b067c1487
Kenneth Jones <kennethjones1991@outlook.com>
2021-02-22 23:47:51 -0700

    Completed browse view and videos view

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Content")
-            .padding()
+        // MARK: - PROPERTIES
+        let animals: [Animal] = Bundle.main.decode("animals.json")
+        
+        // MARK: - BODY
+        NavigationView {
+            List {
+                CoverImageView()
+                    .frame(height: 300)
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                ForEach(animals) { animal in
+                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                        AnimalListItemView(animal: animal)
+                    }
+                }
+            } //: LIST
+            .navigationBarTitle("Africa", displayMode: .large)
+        } //: NAVIGATION
     }
 }
 
---
+++ Africa/Model/VideoModel.swift
@@ -0,0 +1,20 @@
+//
+//  VideoModel.swift
+//  Africa
+//
+//  Created by Kenneth Jones on 2/22/21.
+//
+
+import Foundation
+
+
+struct Video: Codable, Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+    
+    // Computed Property
+    var thumbnail: String {
+        "video-\(id)"
+    }
+}
